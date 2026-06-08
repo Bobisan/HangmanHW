525e8e5e03f2f2b1ad17adcd3caa859f0aa8349b
Bobisan <boris.strinsky@protonmail.com>
2026-06-08 21:48:50 +0300

    Added checks for all input values

--- src/client.c
+++ src/client.c
@@ -5,72 +5,77 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 #include "../include/game.h"
 #include "./game.c"
 
-int main()
+int main(int argc, char const* argv[])
 {
-    int status;
-    char* message = "This is my message";
+
+    if (argc <= 1){
+		perror("\nYou need to specify a host ip address, a port and an opponent word as an argument.\n");
+		return 1;
+	}
+	
+	for(int i=0; i < strlen(argv[2]); i++){
+		if (isdigit(argv[2][i]) < 1){
+			perror("Port argument must be a number.\n");
+			return 1;
+		}
+	}
+
+    for(int i=0; i < strlen(argv[1]); i++){
+		if ((isdigit(argv[1][i]) < 1) && (argv[1][i] != '.')){
+			perror("Address argument must be a number.\n");
+			return 1;
+		}
+	}
+
+
+    char word[strlen(argv[3])+1];
+
+    for(int i=0; i < strlen(argv[3]); i++){
+		word[i] = normalize(argv[3][i]);
+	}
+
+    for(int i=0; i < strlen(word); i++){
+        if (is_letter(word[i]) < 1){
+            perror("Word must contain only letters.\n");
+            return 1;
+        }
+	}
+
+    int port = strtol(argv[2], NULL, 10);
+    char* address = argv[1];
     char buffer[1024] = {0};
     struct sockaddr_in serv_addr;
     ssize_t size = sizeof(buffer);
 	int clientfd = socket(AF_INET, SOCK_STREAM, 0);
-	
-	if (clientfd < 0)
-	{
+
+	if (clientfd < 0){
 		perror("socket failed");
-		return -1;
+		return 1;
 	}
-
+    
+    
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(510);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    serv_addr.sin_port = htons(port);
+    inet_pton(AF_INET, address, &serv_addr.sin_addr);
 
 
-    if ((status = connect(clientfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0){
-        int errsv = errno;
-        printf ("Errno: %d", errsv);
-        return -1;
+    if (connect(clientfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+        perror("\nConnection Failed");
+        return 1;
     }
     
 
-    write(clientfd, message, strlen(message));
-    printf("\nMessage sent");
+    write(clientfd, word, strlen(word));
+    printf("\nword sent");
 
     size_t valread = read(clientfd, buffer, size);
 
     printf("\n%s\n", buffer);
 
     close (clientfd);
-
-    
-
-	/*struct sockaddr_in server_addr = {
-		.sin_family = AF_INET,
-		.sin_port = htons(510),
-	};
-
-	inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
-
-	bind(clientfd, (struct sockaddr *) &server_addr, sizeof(server_addr));*/
-
-
-	/*while (true)
-	{
-		int clientfd = accept(clientfd, NULL, NULL);
-		pid_t child_pid = fork();
-		if (child_pid == 0)
-		{
-			printf("Hello I am %d", getpid());
-			char buffer[1024];
-			ssize_t size;
-			while ((size = read(clientfd, buffer, sizeof(buffer))) > 0)
-				write(clientfd, buffer, size);
-			close(clientfd);
-		}
-	}*/
-
-
 	return 0;
 }
--- src/server.c
+++ src/server.c
@@ -3,25 +3,40 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <ctype.h>
 #include <stdbool.h>
+#include <string.h>
 #include "../include/game.h"
 #include "./game.c"
 
 int main(int argc, char const* argv[])
 {
-	int serverfd = socket(AF_INET, SOCK_STREAM, 0);
 	
+	if (argc <= 1){
+		perror("\nYou need to specify a port as an argument.\n");
+		return 1;
+	}
+	
+	for(int i=0; i < strlen(argv[1]); i++){
+		if (isdigit(argv[1][i]) < 1){
+			perror("\nPort argument must be a number.\n");
+			return 1;
+		}
+	}
+
+	int port = strtol(argv[1], NULL, 10);
+	int serverfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (serverfd < 0)
 	{
-		perror("socket failed");
-		return -1;
+		perror("\nSocket creation failed");
+		return 1;
 	}
 
 	struct sockaddr_in server_addr = {
 		.sin_family = AF_INET,
 		.sin_addr.s_addr = INADDR_ANY,
-		.sin_port = htons(510),
+		.sin_port = htons(port),
 	};
 
 	inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
@@ -29,26 +44,42 @@ int main(int argc, char const* argv[])
 	if (bind(serverfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
 		perror("address binding failed");
 		exit(EXIT_FAILURE);
-	};
-	listen(serverfd, 2);
-	printf("\nlistening ended");
-
-	while (true)
-	{
-		printf("\nI got here");
-		int clientfd = accept(serverfd, NULL, NULL);
-		pid_t child_pid = fork();
-		if (child_pid == 0)
-		{
-			printf("\nHello I am %d", getpid());
-			char buffer[1024];
-			ssize_t size;
-			while ((size = read(clientfd, buffer, sizeof(buffer))) > 0)
-				write(clientfd, buffer, size);
-			close(clientfd);
-		}
 	}
 
+	if(listen(serverfd, 2) < 0){
+		perror("\nListening failed");
+		return 1;
+	}
+
+	printf("Listening on %d...\n", port);
+	int fork_count = 0;
+	while (true)
+	{
+		if(fork_count < 2)
+		{
+			int clientfd = accept(serverfd, NULL, NULL);
+			pid_t child_pid = fork();
+			fork_count++;
+			if (child_pid == 0)
+			{
+				while (true){
+
+				}
+				/*printf("\nHello I am %d", getpid());
+				char buffer[1024];
+				ssize_t size;
+				while ((size = read(clientfd, buffer, sizeof(buffer))) > 0)
+					write(clientfd, buffer, size);*/
+				close(clientfd);
+			}
+			printf("Fork count is %d\n", fork_count);
+		}
+		else
+		{
+			break;
+		}
+	}
+	printf("Client limit reached\n");
 	close(serverfd);
 	return 0;
 }
